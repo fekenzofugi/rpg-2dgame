8d3e6efac98bb760033e6fee75e4a201e863ca01
fekenzofugi <fernandofugihara@gmail.com>
2024-03-14 18:29:31 -0300

    window setup

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        // Causes this Window to be sized to fit the preferred size and layouts
+        // of its subcomponents (=GamePanel)
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setLocation(4000, 500);
+    }
+}
